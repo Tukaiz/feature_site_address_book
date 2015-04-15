73289731bdfc2a004492795890234776f1da60ba
tatums <tatum@ashlandstudios.com>
2015-04-15 10:55:26 -0500

    add default permission

--- lib/feature_site_address_book.rb
+++ lib/feature_site_address_book.rb
@@ -1,6 +1,14 @@
 require "feature_site_address_book/version"
 
 module FeatureSiteAddressBook
+  # This will be executed in the ability class, by defalut, if the Feature is enabled.
+  class Default
+    def self.permissions
+      [
+        "can_access_global_site_address_book"
+      ]
+    end
+  end
   class FeatureSiteAddressBookFeatureDefinition
     include FeatureSystem::Provides
     def permissions
@@ -10,6 +18,11 @@ module FeatureSiteAddressBook
 
   module Authorization
     module Permissions
+
+      def can_access_global_site_address_book
+        can :access_global_site_address_book, Address
+      end
+
     end
   end
 end
